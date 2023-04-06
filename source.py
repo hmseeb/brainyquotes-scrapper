a95056fd9d3d2aa2c74e3aaaa404a0265716ce8f
Haseeb Azhar <haseebazhar6389@gmail.com>
2023-04-06 22:08:48 +0500

    ✍️ Write to CSV

--- source.py
+++ source.py
@@ -1,21 +1,22 @@
+import csv
 from bs4 import BeautifulSoup
 import requests
-html = requests.get('https://www.brainyquote.com/topics')
 
+html = requests.get('https://www.brainyquote.com/topics')
 soup = BeautifulSoup(html.content, "html.parser")
 
 categories = soup.find_all("a", class_="topicIndexChicklet")
 
 titles = []
 urls = []
+quotes = []
+
 for category in categories:
     title = category.find("span", class_="topicContentName").text.strip()
     url = 'https://www.brainyquote.com' + category['href']
     titles.append(title)
     urls.append(url)
 
-authors = []
-quotations = []
 index = 1
 for url in urls:
     try:
@@ -29,10 +30,11 @@ for url in urls:
     for quote in quotes:
         author = quote.find("a", class_="bq-aut")
         quote = quote.find("div", class_="")
-        authors.append(author.text.strip())
-        quotations.append(quote.text.strip())
+        with open('brainyquotes.csv', mode='a', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow([quote.text.strip(), author.text.strip()])
     print(f'Scrapped {url} 😊')
-    pagination_counter = 16
+    pagination_counter = 2
     while (1):
         pagination_url = url + '_' + str(pagination_counter)
         try:
@@ -42,17 +44,17 @@ for url in urls:
         except requests.exceptions.RequestException as e:
             print('Request failed ❌', e)
         if (html.url == url):
+            pagination_counter = 2
             break
         soup = BeautifulSoup(html.content, "html.parser")
         quotes = soup.find_all("div", class_="clearfix")
         for quote in quotes:
             author = quote.find("a", class_="bq-aut")
             quote = quote.find("div", class_="")
-            authors.append(author.text.strip())
-            quotations.append(quote.text.strip())
+            with open('brainyquotes.csv', mode='a', newline='') as file:
+                writer = csv.writer(file)
+                writer.writerow([quote.text.strip(), author.text.strip()])
         print(f'Scrapped {pagination_url} 😊')
-        print(quotations)
-        print(authors)
         pagination_counter += 1
     index += 1
     progress = index / len(urls) * 100
